95eca61e55fbeab58b19c5ac101b3aa7e2736e5e
Kirn Kim <squrkidev@gmail.com>
2024-04-22 17:40:27 -0700

    first commit

---
+++ automation.sh
@@ -0,0 +1,45 @@
+# Automation Task
+
+ 
+
+# Task Description: [Describe the repetitive task that needs to be automated]
+
+# Step 1: Identify the repetitive task to be automated
+# clean and organize ~/Downloads folder
+
+# [Describe the task that you've identified for automation]
+# Move contents to folder named by the date of the download
+ 
+# Step 2: Design and implement the automation script
+
+# Placeholder for Bash script implementation
+
+ 
+
+# Step 3: Test the automation script
+
+ 
+
+# Placeholder for testing instructions
+
+current_datetime=$(date +"%Y-%m-%d")
+
+mkdir ~/Downloads/$current_datetime 
+
+for f in /Downloads/*; do
+  if [ ! -d f ]; then
+  mv f /Downloads/$current_datetime/
+  fi
+done
+
+# Step 4: Document the development process
+
+ 
+
+# Placeholder for documentation
+
+# [Replace this comment with documentation of your script development process]
+
+ 
+
+# End of script
---
+++ practice.sh
@@ -0,0 +1,12 @@
+echo "Welcome to the Bash Scripting Practice!"
+
+echo "Please enter your name: "
+
+read username
+
+echo "Hello, $username! Welcome to the world of Bash scripting."
+
+current_datetime=$(date +"%Y-%m-%d %H:%M:%S")
+
+echo "Current date and time: $current_datetime"
+
